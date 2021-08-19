edab5969d16f1909a4f3abaffcda1a48f3a77de8
Stewart <44583600+stewheart123@users.noreply.github.com>
2021-08-19 08:59:55 +0100

    initial commit

---
+++ Assets/GoogleVR/Demos/Scripts/MaterialGradientModifier.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//use this to stop errors if the script is added to a component without renderer
+[RequireComponent (typeof (Renderer))]
+
+
+public class MaterialGradientModifier : MonoBehaviour
+{
+    Renderer _renderer;
+
+    [SerializeField] Gradient gradient;
+
+    float _gradientPosition = -1;
+    public float gradientPosition
+    {
+        get { return _gradientPosition; }
+        set
+        {
+            if (_gradientPosition != value)
+            {
+                _gradientPosition = value;
+                _renderer.material.color = gradient.Evaluate(_gradientPosition);
+            }
+        }
+    }
+
+    //void SetGradientPosition(float position)
+    //{
+    //    if(position == gradientPosition)
+    //    {
+    //        return;
+    //    }
+    //    gradientPosition = position;
+    //    _renderer.material.color = gradient.Evaluate(gradientPosition);                
+    //}
+
+   // public Color myColor;
+    
+    //starts prior to awake
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+    void Start()
+    {
+        //SetGradientPosition(0);
+        gradientPosition = 0;
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    SetGradientPosition(Mathf.Sin (( (Time.time)) * 0.5f ) + 0.5f);
+    //}
+}
---
+++ Assets/GoogleVR/Demos/Scripts/TouchInuputManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+public class TouchInuputManager : MonoBehaviour
+{
+    [Serializable]
+    public class FloatEvent : UnityEvent<float> { }
+
+    [SerializeField] float delay = 2;
+
+    [SerializeField] UnityEvent onTouch;
+    [SerializeField] UnityEvent onTouchCancel;
+    [SerializeField] AnimationCurve timeUpdateCurve;
+    [SerializeField] FloatEvent onTouchTimerUpdate;
+    [SerializeField] UnityEvent onTouchTimerEnd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            switch( Input.touches[0].phase)
+            {
+                case TouchPhase.Began:
+                    timer = 0;
+                    onTouch.Invoke();
+                    break;
+                case TouchPhase.Ended:
+                    Cancel();
+                    break;
+                default:
+                    timer += Time.deltaTime;
+                    onTouchTimerUpdate.Invoke(timeUpdateCurve.Evaluate(Mathf.InverseLerp(0, delay, timer)));
+                    if (timer > delay)
+                    {
+                        onTouchTimerEnd.Invoke();
+                    }
+                    break;
+            }
+        }
+    }
+
+    void Cancel()
+    {
+        timer = 0;
+        onTouchTimerUpdate.Invoke(timeUpdateCurve.Evaluate(Mathf.InverseLerp(0, delay, timer)));
+        onTouchCancel.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        if(Input.touchCount > 0)
+        {
+            Cancel();
+        }
+        
+    }
+}
